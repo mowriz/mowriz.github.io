3d1ecf6966b86bf81a8d4a6eca78683bc4598e8e
Moritz Limbach <moritz.limbach@hd-solutions.co>
2021-05-20 14:36:12 +0200

    font gulp n stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
-var gulp   = require('gulp');
-var sass   = require('gulp-sass');
-var cleanCSS = require('gulp-clean-css');
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var cleanCSS = require("gulp-clean-css");
 
-gulp.task('build-css', function() {
-    return gulp.src('scss/style.scss')
-        .pipe(sass())
-        .pipe(cleanCSS())
-        .pipe(gulp.dest('css/'));
+gulp.task("build-css", function () {
+  return gulp
+    .src("scss/style.scss")
+    .pipe(sass())
+    .pipe(cleanCSS())
+    .pipe(gulp.dest("css/"));
 });
 
-gulp.task('watch', function() {
-    gulp.watch('scss/**/*.scss', ['build-css']);
+gulp.task("watch", function () {
+  gulp.watch("scss/**/*.scss", ["build-css"]);
 });
